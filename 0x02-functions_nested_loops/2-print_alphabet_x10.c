92e71b2a15e39c1bc423d1eff590e7ea017fe7a3
Tseganes <tsegakebede@gmail.com>
2022-12-13 03:49:38 -0800

    error: expected identifier or ‘(’ before ‘{’ token  in loop in code

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10-print 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
+
+void print_alphabet_x10(void);
+
+{
+	char abc;
+	int i;
+
+	for ((i = 0; i <= 9; i++))
+	{
+		for ((abc = 'a'; abc <= 'z'; abc++))
+		{
+			_putchar(al);
+		}
+		_putchar('\n');
+	}
+}
+
