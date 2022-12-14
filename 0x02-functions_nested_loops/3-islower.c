ce2a258033cf95c6acf83051b8fe177224b5b753
Tseganes <tsegakebede@gmail.com>
2022-12-14 02:16:28 -0800

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,11 +6,12 @@
  *  Return: 1 if letter is lowercase, 0 if not
  */
 
-int _islower(int c);                                                       
+int _islower(int c)
+
 {
-	if (c <= 97 && c <= 122)
-		if (c >= 97 && c <= 122)
+	if (c >= 97 && c <= 122)
+	{
 		return (1);
-	else
+	}
 		return (0);
 }
