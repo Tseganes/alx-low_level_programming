a7147e7782e036ad3b27813512f7e20d50598965
Tseganes <tsegakebede@gmail.com>
2022-12-14 02:32:49 -0800

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,10 +2,15 @@
 
 /**
  * _isapha-checks for alphabetic character
- * @c: 1 if c is a letter, 0 otherwise
+ * @c: The character to be checked
+ * Return: 1 for alphabetic character or 0 for anything else
  */
 
-int_isalpha(int c);
+int _isalpha(int c)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+	if ((c >= 65 && c <= 98) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
 }
