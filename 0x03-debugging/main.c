bfa867333d95f9878bc40a7320ca46b21aa931f6
Tseganes <tsegakebede@gmail.com>
2022-12-15 06:52:59 -0800

    largest

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,37 +1,47 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-void positive_or_negative(int i){
 
-	int n;
+int largest_number(int a, int b, int c)
+{
 
-	srand(time(0));
-	n = i;
-	if (n > 0)
-	{
-	 	printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
-	return (0);
+    int largest;
+
+    if (a > b && a > c)
+    {
+        largest = a;
+    }
+    else if (b > a && b > c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
+
+    return (largest);
 }
+
+	
+
 /**
- * main - debugging
+ * Main - Entry point
  * Return: 0
  */
 
 int main(void)
 {
-	int i;
+	 int a, b, c;
+        int largest;
 
-	i = 0;
-	        positive_or_negative(i);
+        a = 972;
+        b = -98;
+        c = 0;
+
+        largest = largest_number(a, b, c);
+
+        printf("%d is the largest number\n", largest);
 
         return (0);
 }
