35356bba17497085b7d98705aa41b9f9adcfb5b7
Tseganes <tsegakebede@gmail.com>
2022-12-15 07:13:09 -0800

    task d

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -1,7 +1,12 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 
+/**
+* convert_day - converts day of month to day of year, without accounting
+* for leap year
+* @month: month in number format
+* @day: day of month
+* Return: day of year
+*/
 
 int convert_day(int month, int day)
 {
@@ -44,44 +49,4 @@ int convert_day(int month, int day)
             break;
     }
     return (day);
-}
-
-void print_remaining_days(int month, int day, int year)
-{
-    if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
-    {
-        if (month >= 2 && day >= 60)
-        {
-            day++;
-        }
-
-        printf("Day of the year: %d\n", day);
-        printf("Remaining days: %d\n", 366 - day);
-    }
-    else
-    {
-        if (month == 2 && day == 60)
-        {
-            printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-        }
-        else
-        {
-            printf("Day of the year: %d\n", day);
-            printf("Remaining days: %d\n", 365 - day);
-        }
-    }
-}
-
-	
-
-/**
- * Main - Entry point
- * Return: 0
- */
-
-int main(void)
-{
-	int day = convert_day(04,01);
-	print_remaining_day(4,day,1997);
-		return (0);
 }
