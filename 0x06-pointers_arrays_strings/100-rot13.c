0a8a989d25249d5331776712170b178a1cc95297
tseganes <tsegakebede20@gmail.com>
2022-12-23 01:43:32 -0800

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,33 +6,21 @@
  */
 char *rot13(char *str)
 {
-	int indx1 = 0, indx2;
+	int i;
+	int j;
 
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-		'G', 'H', 'I', 'J', 'K', 'L', 
-		'M', 'N', 'O', 'P', 'Q', 'R',
-		'S', 'T', 'U', 'V', 'W', 'X', 
-		'Y', 'Z'};
-	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-		'T', 'U', 'V', 'W', 'X', 'Y', 
-		'Z', 'A', 'B', 'C', 'D', 'E', 
-		'F', 'G', 'H', 'I', 'J', 'K', 
-		'L', 'M', 'n', 'o', 'p', 'q', 
-		'r', 's', 't', 'u', 'v', 'w',
-		'x', 'y', 'z', 'a', 'b', 'c', 
-		'd', 'e', 'f', 'g', 'h', 'i', 
-		'j', 'k', 'l', 'm'};
-	while (str[indx1])
-	{
-		for (indx2 = 0; indx2 < 52; indx2++)
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghiklm";
+		for (i = 0; str[i] != '\0'; i++)
 		{
-			if (str[indx1] == alphabet[indx2])
+			for (j = 0; j < 52; j++)
 			{
-				str[indx1] = rot13key[indx2];
-				break;
+				if (str[i] == data1[j])
+				{
+					str[i] = datarot[j];
+					break;
+				}
 			}
 		}
-		indx1++;
-	}
 	return (str);
 }
