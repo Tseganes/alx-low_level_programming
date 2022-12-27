632be38c89fad70b9149d3a83f50ae4b788e4382
tseganes <tsegakebede20@gmail.com>
2022-12-26 23:04:19 -0800

    hot fix

--- 0x04-more_functions_nested_loops/6-main.c
+++ 0x04-more_functions_nested_loops/6-main.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * main - check the code
- * 
+ *
  * Return: Always 0.
  */
-int main(void )
+int main(void)
 {
-	print _line(0);
+	print_line(0);
 	print_line(2);
 	print_line(10);
 	print_line(-4);
