25199c742fd5eb9c0f83153eadcdcef9c51b60e0
tseganes <tsegakebede20@gmail.com>
2023-01-18 02:18:18 -0800

    task

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+/**
+ * op_add - calculates the sum
+ * @a: int
+ * @b: integer
+ *
+ * Return: sum 
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - calculates difference
+ * @a: integer
+ * @b: second
+ *
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - calculates product
+ * @a: integer 
+ * @b: integer
+ *
+ * Return: product
+ */ 
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - calculates division
+ * @a: integer
+ * @b: integer
+ *
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - calculates remainder
+ * @a: integer
+ * @b: integer
+ *
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
